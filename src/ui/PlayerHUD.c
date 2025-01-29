2dca5b028b26e81cd2cf455c502bd890256f0b12
John Smith <187957415+chudware@users.noreply.github.com>
2025-01-29 22:48:07 +0100

    added stone guardian hp bar

--- src/ui/PlayerHUD.c
+++ src/ui/PlayerHUD.c
@@ -5,6 +5,7 @@
 #include "../player/Player.h"
 
 extern Entity player;
+extern Entity StoneGuardian;
 Texture2D stats;
 Texture2D gear;
 
@@ -27,4 +28,13 @@ void DrawPlayerHUD()
     DrawRectangle(screenWidth - 330, screenHeight - 36, player.xp, 10, ORANGE);
     DrawRectangle(screenWidth - 162, screenHeight - 36, player.hp, 10, RED);
     DrawRectangle(screenWidth - 162, screenHeight - 50, player.mp, 10, BLUE);
+
+    // draw hp bar
+    float stoneGuardianHP = StoneGuardian.hp;
+    if (stoneGuardianHP > 0)
+    {
+        DrawRectangle(screenWidth / 2 - 45000 / 100 / 2 - 1, 15, 45000 / 100 + 2, 18, BLACK);             // bar outline
+        DrawRectangle(screenWidth / 2 - 45000 / 100 / 2, 16, 45000 / 100, 16, RED);                       // empty bar color
+        DrawRectangle(screenWidth / 2 - stoneGuardianHP / 100 / 2, 16, stoneGuardianHP / 100, 16, GREEN); // health bar
+    }
 }
